31f7680257a9cd03b9a4ab584e00b47ba44c95cb
Ray Karyshyn <raykaryshyn@gmail.com>
2023-03-02 18:16:06 -0500

    C447-2 add bootstrap and HTML templates, python formatting libs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Home page.'
+    return render_template('home.html')
