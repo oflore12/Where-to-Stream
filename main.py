0b959d57e8ad3e53d9795e339a3a8e1409b855c0
Ray Karyshyn <raykaryshyn@gmail.com>
2023-04-10 20:38:08 -0400

    main.py comments, refactoring

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from urllib.request import urlopen
-import json
 import tmdbsimple as tmdb
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
@@ -12,14 +10,17 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://wts:team3@localhost:5432/w
 db = SQLAlchemy(app)
 
 tmdb.API_KEY = "523e00cfc7fcc6bed883c38162ea974d"
-# Recommended by the tmdbsimple devs, so if the site is down the code won't get stuck there
+# As recommended by the tmdbsimple developers, this timeout ensures the code won't get stuck if TMDb is down
 tmdb.REQUESTS_TIMEOUT = 5
+
+# Defines the expiration time for cached API results
+CACHE_CLOCK = "1 minute"
 
 
 class TVResult(db.Model):
     __tablename__ = 'TVResults'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    title = db.Column(db.String(100))
+    title = db.Column(db.String(500))
     providers = db.Column(SQL_JSON)
     last_updated = db.Column(db.DateTime(timezone=True),
                              server_default=db.func.now(), onupdate=db.func.current_timestamp())
@@ -31,7 +32,7 @@ class TVResult(db.Model):
 class MovieResult(db.Model):
     __tablename__ = 'MovieResults'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    title = db.Column(db.String(100))
+    title = db.Column(db.String(500))
     providers = db.Column(SQL_JSON)
     last_updated = db.Column(db.DateTime(timezone=True),
                              server_default=db.func.now(), onupdate=db.func.current_timestamp())
@@ -43,7 +44,7 @@ class MovieResult(db.Model):
 class Query(db.Model):
     __tablename__ = 'Queries'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    q = db.Column(db.String(100), nullable=False)
+    q = db.Column(db.String(500), nullable=False)
 
     def __repr__(self):
         return f'<Query: {self.q}>'
@@ -79,26 +80,36 @@ def home():
 def search():
     if request.method == 'GET':
         q = request.args.get('q')
-        if q is None or q == '':
+        # If 'q' is empty or not provided, redirect to home page
+        if not q:
             return redirect(url_for('home'))
+
+        # Remove whitespace from 'q'
         q = q.strip()
+
         provider = request.args.get('provider')
+
         results = getResults(q)
+
         return render_template("search.html", q=q, provider=provider, results=results, country="US")
 
     return redirect(url_for('home'))
 
 
+# The function takes a search query and returns a list of results
 def getResults(q):
     search = tmdb.Search()
+    # Check if the query is already cached in the 'Query' table
     cached_q = Query.query.filter_by(q=q).first()
     results = []
 
-    if cached_q is None:
+    # If query not cached
+    if not cached_q:
         newQuery = Query(q=q)
         db.session.add(newQuery)
         db.session.commit()
 
+        # Search using the 'tmdb' API (primarily for retrieving IDs)
         search.multi(query=q)
 
         for result in search.results:
@@ -109,90 +120,111 @@ def getResults(q):
             media_type = result["media_type"]
             title = result["title"] if media_type == "movie" else result["name"]
 
-            tv_id = -1
-            movie_id = -1
-            exists = None
-            if media_type == "tv":  # TV
-                tv_id = id
-                exists = TVResult.query.filter_by(id=tv_id).first()
-                providerSearch = tmdb.TV(tv_id)
-                providerSearch.watch_providers()
-                newResult = TVResult(
-                    id=tv_id, title=title, providers=providerSearch.results)
-            else:  # Movie
-                movie_id = id
-                exists = MovieResult.query.filter_by(id=movie_id).first()
-                providerSearch = tmdb.Movies(movie_id)
-                providerSearch.watch_providers()
-                newResult = MovieResult(
-                    id=movie_id, title=title, providers=providerSearch.results)
+            # If media type is TV
+            if media_type == "tv":
+                # Check if the TV result is already cached
+                exists = TVResult.query.filter_by(id=id).first()
+                # If not cached
+                if not exists:
+                    # Retrieve watch providers and create new 'TVResult' instance
+                    providerSearch = tmdb.TV(id)
+                    providerSearch.watch_providers()
+                    newResult = TVResult(
+                        id=id, title=title, providers=providerSearch.results)
+                # If cached, perform TV caching procedure
+                else:
+                    newResult = TVCache(id)
+            # If media type is movie
+            else:
+                # Check if the movie result is already cached
+                exists = MovieResult.query.filter_by(id=id).first()
+                # If not cached
+                if not exists:
+                    # Retrieve watch providers and create new 'MovieResult' instance
+                    providerSearch = tmdb.Movies(id)
+                    providerSearch.watch_providers()
+                    newResult = MovieResult(
+                        id=id, title=title, providers=providerSearch.results)
+                # If cached, perform movie caching procedure
+                else:
+                    newResult = MovieCache(id)
 
             results.append(newResult)
 
-            if (not exists):
+            # If new result not cached, add to the database and commit change
+            if not exists:
                 db.session.add(newResult)
                 db.session.commit()
 
+            # Create a new 'QueryResultMapping' instance and add to the database and commit changes
             newQueryResultMapping = QueryResultMapping(
-                tv_result=tv_id, movie_result=movie_id, q=newQuery.id)
+                tv_result=id if media_type == "tv" else -1,
+                movie_result=id if media_type == "movie" else -1,
+                q=newQuery.id)
 
             db.session.add(newQueryResultMapping)
             db.session.commit()
+    # If query is cached
     else:
+        # Retrieve cached result IDs from the 'QueryResultMapping' table
         cached_result_ids = QueryResultMapping.query.filter_by(
             q=cached_q.id).all()
 
+        # Perform caching procedure for each result
         for cached_result_id in cached_result_ids:
-            currentResult = None
+            # If the cached result is a TV result
+            if cached_result_id.tv_result != -1:
+                currentResult = TVCache(cached_result_id.tv_result)
+            # If the cached result is a movie result
+            else:
+                currentResult = MovieCache(cached_result_id.movie_result)
 
-            if cached_result_id.tv_result != -1:  # TV
-                currentResult = db.session.query(TVResult).from_statement(text("""
-                    SELECT
-                        "TVResults".id AS "TVResults_id", "TVResults".title AS "TVResults_title",
-                        "TVResults".providers AS "TVResults_providers", "TVResults".last_updated AS "TVResults_last_updated"
-                    FROM "TVResults"
-                    WHERE "TVResults".id = %(id_1)s
-                        AND "TVResults".last_updated > CURRENT_TIMESTAMP - interval '1 day'
-                    """ % {'id_1': cached_result_id.tv_result})).first()
-
-                if (not currentResult):
-                    recacheSearch = tmdb.TV(cached_result_id.tv_result)
-                    recacheSearch.watch_providers()
-                    currentResult = db.session.query(TVResult).from_statement(text("""
-                        SELECT
-                            "TVResults".id AS "TVResults_id", "TVResults".title AS "TVResults_title",
-                            "TVResults".providers AS "TVResults_providers", "TVResults".last_updated AS "TVResults_last_updated"
-                        FROM "TVResults"
-                        WHERE "TVResults".id = %(id_1)s
-                        """ % {'id_1': cached_result_id.tv_result})).first()
-                    currentResult.providers = recacheSearch.results
-                    currentResult.last_updated = db.func.current_timestamp()
-                    db.session.commit()
-            else:  # Movie
-                currentResult = db.session.query(MovieResult).from_statement(text("""
-                    SELECT
-                        "MovieResults".id AS "MovieResults_id", "MovieResults".title AS "MovieResults_title",
-                        "MovieResults".providers AS "MovieResults_providers", "MovieResults".last_updated AS "MovieResults_last_updated"
-                    FROM "MovieResults"
-                    WHERE "MovieResults".id = %(id_1)s
-                        AND "MovieResults".last_updated > CURRENT_TIMESTAMP - interval '1 day'
-                    """ % {'id_1': cached_result_id.movie_result})).first()
-
-                if (not currentResult):
-                    recacheSearch = tmdb.Movies(cached_result_id.movie_result)
-                    recacheSearch.watch_providers()
-                    currentResult = db.session.query(MovieResult).from_statement(text("""
-                        SELECT
-                            "MovieResults".id AS "MovieResults_id", "MovieResults".title AS "MovieResults_title",
-                            "MovieResults".providers AS "MovieResults_providers", "MovieResults".last_updated AS "MovieResults_last_updated"
-                        FROM "MovieResults"
-                        WHERE "MovieResults".id = %(id_1)s
-                        """ % {'id_1': cached_result_id.movie_result})).first()
-                    currentResult.providers = recacheSearch.results
-                    currentResult.last_updated = db.func.current_timestamp()
-                    db.session.commit()
-
-            if (currentResult):
-                results.append(currentResult)
+            results.append(currentResult)
 
     return results
+
+
+def TVCache(id):
+    # Check if there is a cached result in the database that is not "stale"
+    currentResult = db.session.query(TVResult).from_statement(text("""
+        SELECT
+            "TVResults".id AS "TVResults_id", "TVResults".title AS "TVResults_title",
+            "TVResults".providers AS "TVResults_providers", "TVResults".last_updated AS "TVResults_last_updated"
+        FROM "TVResults"
+        WHERE "TVResults".id = %(id_1)s
+            AND "TVResults".last_updated > CURRENT_TIMESTAMP - interval '%(cacheClock_1)s'
+        """ % {'id_1': id, 'cacheClock_1': CACHE_CLOCK})).first()
+
+    # If the cached result is stale, retrieve the latest provider data and update the cache
+    if (not currentResult):
+        recacheSearch = tmdb.TV(id)
+        recacheSearch.watch_providers()
+        currentResult = TVResult.query.filter_by(id=id).first()
+        currentResult.providers = recacheSearch.results
+        currentResult.last_updated = db.func.current_timestamp()
+        db.session.commit()
+
+    return currentResult
+
+
+def MovieCache(id):
+    # Check if there is a cached result in the database that is not "stale"
+    currentResult = db.session.query(MovieResult).from_statement(text("""
+        SELECT
+            "MovieResults".id AS "MovieResults_id", "MovieResults".title AS "MovieResults_title",
+            "MovieResults".providers AS "MovieResults_providers", "MovieResults".last_updated AS "MovieResults_last_updated"
+        FROM "MovieResults"
+        WHERE "MovieResults".id = %(id_1)s
+            AND "MovieResults".last_updated > CURRENT_TIMESTAMP - interval '%(cacheClock_1)s'
+        """ % {'id_1': id, 'cacheClock_1': CACHE_CLOCK})).first()
+
+    # If the cached result is stale, retrieve the latest provider data and update the cache
+    if (not currentResult):
+        recacheSearch = tmdb.Movies(id)
+        recacheSearch.watch_providers()
+        currentResult = MovieResult.query.filter_by(id=id).first()
+        currentResult.providers = recacheSearch.results
+        currentResult.last_updated = db.func.current_timestamp()
+        db.session.commit()
+
+    return currentResult
